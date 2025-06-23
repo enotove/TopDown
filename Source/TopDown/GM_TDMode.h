c67bfa6a6b7bc6099b162686ae62e47d1254c412
Enotove <enotove@gmail.com>
2025-06-23 14:07:37 +0500

    Init commit

---
+++ Source/TopDown/GM_TDMode.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameStateBase.h"
+#include "GM_TDMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TOPDOWN_API AGM_TDMode : public AGameStateBase
+{
+	GENERATED_BODY()
+	
+};
